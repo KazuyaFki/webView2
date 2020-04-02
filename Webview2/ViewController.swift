4ae5ee286319cc547fc9a90d0a72fbb32e85af2e
KazuyaFki <complete501xx@yahoo.co.jp>
2020-04-02 19:43:35 +0900

    Initial Commit

---
+++ Webview2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Webview2
+//
+//  Created by Kazuya Fukui on 2020/04/02.
+//  Copyright © 2020 Kazuya Fukui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
